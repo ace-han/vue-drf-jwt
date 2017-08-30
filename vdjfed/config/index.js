c81ce1faf5ae9e64f66d6e94052caa97cf2479d8
Ace <ace.jl.han@gmail.com>
2017-08-31 01:18:12 +0800

    add new dependencies, proxy and chunkfilename settings to fed build scripts

--- vdjfed/config/index.js
+++ vdjfed/config/index.js
@@ -27,7 +27,13 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    // will be used in /build/dev-server.js
+    proxyTable: {
+      '/api/**': {
+        target: process.env.backend_endpoint || 'http://localhost:8090',
+        changeOrigin: true
+      },
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
