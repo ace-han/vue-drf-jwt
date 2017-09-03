2241e8ca2af7540eed4633954de3e0599d4df9d7
Ace <ace.jl.han@gmail.com>
2017-09-04 02:06:28 +0800

    fix `@` error in webpack config

--- vdjfed/build/webpack.base.conf.js
+++ vdjfed/build/webpack.base.conf.js
@@ -5,7 +5,7 @@ var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
-  return path.join('..', dir)
+  return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
